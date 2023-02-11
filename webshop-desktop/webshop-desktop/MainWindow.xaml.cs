01db16821a23ec2eb58a0ddf42e22696033d4968
kopcsakpatrik <kopcsakpatrik1@gmail.com>
2023-02-11 15:35:43 +0100

    titlebar

--- webshop-desktop/webshop-desktop/MainWindow.xaml.cs
+++ webshop-desktop/webshop-desktop/MainWindow.xaml.cs
@@ -16,24 +16,15 @@ using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 
-// To learn more about WinUI, the WinUI project structure,
-// and more about our project templates, see: http://aka.ms/winui-project-info.
+namespace webshop_desktop;
 
-namespace webshop_desktop
+public sealed partial class MainWindow : Window
 {
-    /// <summary>
-    /// An empty window that can be used on its own or navigated to within a Frame.
-    /// </summary>
-    public sealed partial class MainWindow : Window
+    public MainWindow()
     {
-        public MainWindow()
-        {
-            this.InitializeComponent();
-        }
-
-        private void myButton_Click(object sender, RoutedEventArgs e)
-        {
-            myButton.Content = "Clicked";
-        }
+        this.InitializeComponent();
+        Title = "Webshop";
+        ExtendsContentIntoTitleBar = true;
+        SetTitleBar(TitleBar);
     }
 }
