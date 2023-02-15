6b5aca12d5b78b9062a70092bda891151f4b0ea0
Varga <vargazsigmond02@gmail.com>
2023-02-15 20:11:14 +0100

    Navbar design

--- webshop-frontend/src/components/Navbar.jsx
+++ webshop-frontend/src/components/Navbar.jsx
@@ -1,4 +1,6 @@
 import { AccountCircle, LocalMall, Search } from '@mui/icons-material'
+import { Badge } from '@mui/material'
+import { red } from '@mui/material/colors'
 import React from 'react'
 import styled from 'styled-components'
 
@@ -10,20 +12,21 @@ const Wrapper = styled.div`
    background-color: #212121;
    color: #ffa1ff;
    height: 80px;
-   
+   border-bottom: 2px solid #ffa1ff;
 `
 const Left = styled.div`
-    flex:1;
+    flex:0.5;
     display: flex;
     align-items: center;
 `
 const Center = styled.div`
-    flex:1;
+    flex:1.5;
     display: flex;
     align-items: center;
     font-weight: bold;
     font-size: 40px;
-    justify-content: center;
+    
+    
     
 `
 const Right = styled.div`
@@ -31,17 +34,50 @@ const Right = styled.div`
     display: flex;
     align-items: center;
     justify-content: right;
+    word-spacing: 20px;
     cursor: pointer;
+    
     
 `
 const Language =  styled.div`
     font-size:20px;
     cursor:pointer;
+    margin-right: 30px;
 `
 
 const Logo = styled.div`
-text-align: left;
+ flex:1;
+ margin-right: 30px;
+ align-items: center;
+
 `
+const Linkek = styled.div`
+justify-content: right;
+font-size: 20px;
+word-spacing: 20px;
+cursor: pointer;
+align-items: center;
+display: flex;
+`
+const MenuIcons = styled.div`
+cursor: pointer;
+align-items: center;
+font-size: 130px;
+display: flex;
+margin-left: 25px;
+
+`
+const StyledBadge = styled(Badge)(({ theme }) => ({
+    '& .MuiBadge-badge': {
+        right: 5,
+        top: 5,
+        border: `2px solid ${red}`,
+       
+        fontSize: `20px`,
+    },
+  }));
+  
+
 
 //const Icon = styled(AccountCircle)`
 //  color: black;
@@ -56,12 +92,17 @@ const Navbar = () => {
             <Language>HU</Language>
            
         </Left>
-        <Center>FootFrenzy</Center>
+        <Center><Logo>FootFrenzy</Logo><Linkek> FŐOLDAL SNEAKEREK RELEASES INFO</Linkek>  </Center>
         <Right>
-                
-                <AccountCircle></AccountCircle>
-                <Search></Search>
-                <LocalMall></LocalMall>    
+                <MenuIcons>
+                               
+                  
+                <AccountCircle style={{fontSize:"40px", marginLeft:"10px", marginRight:"10px"}}></AccountCircle>
+                <Search style={{fontSize:"40px", marginLeft:"10px", marginRight:"10px"}}></Search>
+                <StyledBadge badgeContent={4} color="default">
+                <LocalMall style={{fontSize:"40px", marginLeft:"10px", marginRight:"10px"}} color="inherit"></LocalMall> 
+                </StyledBadge>  
+                </MenuIcons>   
         </Right>
       </Wrapper>
     
