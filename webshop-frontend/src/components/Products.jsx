a0a1b56d2b053fbf06dd0c97c51c3590a5bb0ac9
Varga <vargazsigmond02@gmail.com>
2023-02-27 19:52:42 +0100

    bestseller

--- webshop-frontend/src/components/Products.jsx
+++ webshop-frontend/src/components/Products.jsx
@@ -7,6 +7,7 @@ const Container = styled.div`
     padding: 20px;
     padding-left: 11vw;
     padding-right: 11vw;
+    padding-bottom: 100px;
     display: flex;
     flex-wrap: wrap;
     justify-content: space-between;
