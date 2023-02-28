0fb4f7aa0fcb4b3178f16f5a9f0c56affe0b7ae4
Varga Zsigmond <vargazsigmond02@gmail.com>
2023-02-28 10:21:52 +0100

    new product menu

--- webshop-frontend/src/pages/Home.jsx
+++ webshop-frontend/src/pages/Home.jsx
@@ -6,15 +6,19 @@ import Categories from '../components/Categories'
 import Products from "../components/Products";
 import Newsletter from '../components/Newsletter';
 import Footer from '../components/Footer';
+import NewProducts from '../components/newProducts';
+
 const Home = () => {
   return (
     <div>
       <Navbar/>
       <Slideshow/>
       <Categories />
+      <NewProducts/>
       <Products/>
       <Newsletter/>
       <Footer/>
+      
     </div>
   )
 }
