b04c11b0b4742629f403913f707ffeab9106647f
KOPCSAKPATRIK1 <kopcsakpatrik1@gmail.com>
2023-03-30 16:36:09 +0200

    changeaddress iskolaban tortent

---
+++ webshop-desktop/Webshop.Desktop/ViewModels/Dialogs/ChangeAddress.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace Webshop.Desktop.ViewModels.Dialogs;
+public partial class ChangeAddress
+{
+    [ObservableProperty] private string _streetAddress;
+}
