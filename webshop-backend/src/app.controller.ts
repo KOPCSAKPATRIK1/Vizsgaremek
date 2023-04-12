8920eb1369ed00095aed1b42bdf2375346d7e5fb
polgar kristof <polgarkristof10@gmail.com>
2023-04-12 14:22:10 +0200

    stock kész, inactiv hozzáadva

--- webshop-backend/src/app.controller.ts
+++ webshop-backend/src/app.controller.ts
@@ -34,6 +34,7 @@ import { PaymentMethod } from './entities/order/paymentMethod.entity';
 import AddressDto from './dto/address.dto';
 import { Address } from './entities/user/address.entity';
 import OrderDto from './dto/order.dto';
+import { Stock } from './entities/product/stock.entity';
 
 @Controller()
 export class AppController {
@@ -121,6 +122,7 @@ export class AppController {
       .createQueryBuilder('product')
       .leftJoinAndSelect('product.category', 'category')
       .leftJoinAndSelect('product.sizes', 'size')
+      .where('product.inactive = :inactive', { inactive: 0 })
       .getMany();
   }
   
@@ -134,13 +136,13 @@ export class AppController {
 
   @Get('/shoes/popular')
   async getPopularShoes() {
-    const productRepo = this.dataSource.getRepository(Product);
-    return productRepo
-      .createQueryBuilder('product')
-      .leftJoinAndSelect('product.category', 'category')
-      .where('product.popular = :popular', { popular: true })
-      .getMany();
-  }
+  const productRepo = this.dataSource.getRepository(Product);
+  return productRepo
+    .createQueryBuilder('product')
+    .leftJoinAndSelect('product.category', 'category')
+    .where('product.popular = :popular and product.inactive = :inactive', { popular: true, inactive: 0 })
+    .getMany();
+}
 
   @Get('/shoes/:id')
   async getShoe(@Param('id') id: number) {
@@ -157,7 +159,7 @@ export class AppController {
     const productRepo = this.dataSource.getRepository(Product);
     const shoes = await productRepo
       .createQueryBuilder('product')
-      .where('product.name LIKE :name', { name: `%${name}%` })
+      .where('product.name LIKE :name and product.inactive = :inactive', { name: `%${name}%`, inactive: 0 })
       .getMany(); 
     return shoes;
   }
@@ -329,5 +331,10 @@ export class AppController {
     return order;
   }
   
+  @Get('stock/:productId')
+  getStocks(@Param('productId') productId : number){
+    const stockRepo = this.dataSource.getRepository(Stock);
+    return stockRepo.findBy({ productId : productId});
+  }
   
 }
