8f1652868180efaed467224eab96e8f78b47e834
Varga <vargazsigmond02@gmail.com>
2023-04-24 03:16:08 +0200

    strict mode off

--- webshop-frontend/src/index.js
+++ webshop-frontend/src/index.js
@@ -9,7 +9,7 @@ import {store, persistor} from './redux/store'
 import { PersistGate } from 'redux-persist/integration/react'
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+ // <React.StrictMode>
     <Provider store={store}>
     <PersistGate loading={null} persistor={persistor}>
     <BrowserRouter>
@@ -18,7 +18,7 @@ root.render(
     </BrowserRouter>
     </PersistGate>
     </Provider>
-  </React.StrictMode>
+ // </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
